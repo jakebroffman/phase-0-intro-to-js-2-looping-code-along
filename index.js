3cfbabdfd25a96fbbfcc563519fdd2aa4973b737
jakebroffman <jake.broffman1088@gmail.com>
2022-11-11 11:36:38 -0500

    commit tree c10949e2e5a88fe486a22e375934792488e121a8

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names) {
+    const event = "surprise";
+    const message = [];
+    for (let i = 0; i < names.length; i++) {
+       message.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+
+    }
+    return message;
+}
+
+writeCards(names);
+
+function countDown () {
+    let countDown = 10;
+    while (countDown > -1) {
+        console.log(countDown--);
+    }
+}
